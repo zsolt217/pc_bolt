fe475ed063d34a22541c3d433c551466b45a60b6
zsolt217 <zsoltfulop217@gmail.com>
2016-04-13 18:34:38 +0200

    Revert "ugyanaz"
    This reverts commit 2a39c2dad51fc821b4b33e38d4e2d3d196d2f3ae.

---
+++ Szt2_projekt/BejelentkezoVM.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Szt2_projekt
+{
+    class BejelentkezoVM
+    {
+
+        private FELHASZNALO aktualisFelhasznalo;
+
+        public string AktualisFelhasznaloID
+        {
+            get
+            {
+                return aktualisFelhasznalo.FELHASZNALO_ID.ToString();
+            }
+        }
+
+        public string AktualisFelhasznaloNev
+        {
+            get
+            {
+                return aktualisFelhasznalo.NEV;
+            }
+        }
+        private AdatbazisEntities1 db; 
+
+        public BejelentkezoVM()
+        {
+
+            db = new AdatbazisEntities1();
+
+            aktualisFelhasznalo = new FELHASZNALO();
+
+        }
+
+        public bool Bejelentkezes(string felhasznalonev, string jelszo)
+        {
+            FELHASZNALO f = this.TartalmazasVizsgalat(felhasznalonev, jelszo);
+            if (f != null)
+            {
+                aktualisFelhasznalo = f;
+            }
+
+            return f != null;
+        }
+
+        private bool TartalmazasVizsgalat(string felhasznalonev)
+        {
+            int tartalmaz = db.FELHASZNALO.Count(x => x.NEV.Equals(felhasznalonev.ToUpper()));
+            return tartalmaz > 0;
+        }
+
+        private FELHASZNALO TartalmazasVizsgalat(string felhasznalonev, string jelszo)
+        {
+            var z = from f in db.FELHASZNALO
+                    where f.NEV.Equals(felhasznalonev.ToUpper())
+                    select f;
+
+            return z.First();
+
+            //var zu = db.FELHASZNALO.Where(x => x.NEV.Equals(felhasznalonev.ToUpper()) && x.JELSZO.Equals(jelszo.ToUpper())).First();
+            //return f;
+        }
+
+        protected void Regisztracio()
+        {
+
+        }
+
+
+    }
+}
