2a39c2dad51fc821b4b33e38d4e2d3d196d2f3ae
zsolt217 <zsoltfulop217@gmail.com>
2016-04-13 18:23:22 +0200

    ugyanaz

---
+++ SharedSource/Megosztott.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szt2_projekt
+
+{
+    public static class Megosztott
+    {
+        public static void LogIras(string szoveg)
+        {
+            using (System.IO.StreamWriter file =
+                      new System.IO.StreamWriter("log.txt", true))
+            {
+                
+                file.WriteLine(DateTime.Now.ToString() + Environment.NewLine + szoveg);
+            }
+        }
+    }
+}
