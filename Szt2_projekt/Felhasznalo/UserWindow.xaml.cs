1951f7d5e2e9c159beada447e2e212175373c284
Metalhulk <dkristoff@hotmail.com>
2016-04-21 20:54:56 +0200

    <felhasználó> konfignál adatmegjelenítés done

--- Szt2_projekt/Felhasznalo/UserWindow.xaml.cs
+++ Szt2_projekt/Felhasznalo/UserWindow.xaml.cs
@@ -206,88 +206,380 @@ namespace Szt2_projekt
         }
         #endregion
 
-
+        void LabelNullaz()
+        {
+            lbl11.Content = string.Empty;
+            lbl12.Content = string.Empty;
+            lbl13.Content = string.Empty;
+            lbl14.Content = string.Empty;
+            lbl15.Content = string.Empty;
+            lbl16.Content = string.Empty;
+            lbl17.Content = string.Empty;
+        } // az adatlabeleket nulláza
         AdatbazisEntities ab;
         private void rbAlaplap_Checked(object sender, RoutedEventArgs e)
         {
-
+            LabelNullaz();
             for (int i = 0; i < alaplaplabelnevek.Length; i++)
             {
                 labelek[i].Content = alaplaplabelnevek[i];
             }
-
-            if (cBoxAlaplap.SelectedIndex != cBoxAlaplap.Items.Count) //ha nem az utolsó van kiválasztva
+            if (cBoxAlaplap.SelectedIndex == cBoxAlaplap.Items.Count - 1)
             {
-                ab = new AdatbazisEntities();
-                ALAPLAP alap = new ALAPLAP();
-
-                string keres = cBoxAlaplap.SelectedItem.ToString();
-                var q = from akt in ab.ALAPLAP
-                        where akt.TIPUSSZAM == keres
-                        select akt;
-
-
-                alap = q.FirstOrDefault();
-                //AlkatreszAdatokGrid.DataContext = alap;
-
+                LabelNullaz();
             }
+            else
+            {
+                ALAPLAP aktalaplap = new ALAPLAP();
+                aktalaplap = (ALAPLAP)cBoxAlaplap.SelectedValue;
+
+                lbl11.Content = aktalaplap.AR;
+                lbl12.Content = aktalaplap.TIPUSSZAM;
+                lbl13.Content = aktalaplap.CPUFOGLALAT;
+                lbl14.Content = aktalaplap.MEMORIASLOTOK;
+                lbl15.Content = aktalaplap.MEMORIATIPUS;
+                lbl16.Content = aktalaplap.CHIPSET;
+                lbl17.Content = aktalaplap.MERETSZABVANY;
+            }
+
         }
 
         private void rbWinchester_Checked(object sender, RoutedEventArgs e)
         {
+            LabelNullaz();
             for (int i = 0; i < tarololabelnevek.Length; i++)
             {
                 labelek[i].Content = tarololabelnevek[i];
+            }
+            if (cBoxHDD.SelectedIndex == cBoxHDD.Items.Count - 1)
+            {
+                LabelNullaz();
+            }
+            else
+            {
+                HDD aktHDD = new HDD();
+
+                aktHDD = (HDD)cBoxHDD.SelectedValue;
+
+                lbl11.Content = aktHDD.AR;
+                lbl12.Content = aktHDD.TIPUSSZAM;
+                lbl13.Content = aktHDD.KAPACITAS;
             }
         }
 
 
         private void rbProcesszor_Checked(object sender, RoutedEventArgs e)
         {
+            LabelNullaz();
             for (int i = 0; i < proclabelnevek.Length; i++)
             {
                 labelek[i].Content = proclabelnevek[i];
+            }
+            if (cBoxCPU.SelectedIndex == cBoxCPU.Items.Count - 1)
+            {
+                LabelNullaz();
+            }
+            else
+            {
+                CPU akt = new CPU();
+
+                akt = (CPU)cBoxCPU.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.CPUFOGLALAT;
+                lbl14.Content = akt.FOGYASZTAS;
+                lbl15.Content = akt.SEBESSEG;
+                lbl16.Content = akt.MAGOK;
             }
         }
 
         private void rbVideokartya_Checked(object sender, RoutedEventArgs e)
         {
+            LabelNullaz();
             for (int i = 0; i < gpulabelnevek.Length; i++)
             {
                 labelek[i].Content = gpulabelnevek[i];
+            }
+            if (cBoxGPU.SelectedIndex == cBoxGPU.Items.Count - 1)
+            {
+                LabelNullaz();
+            }
+            else
+            {
+                GPU akt = new GPU();
+
+                akt = (GPU)cBoxGPU.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.FOGYASZTAS;
+                lbl14.Content = akt.MEMORIA;
             }
         }
 
 
         private void rbMemoria_Checked(object sender, RoutedEventArgs e)
         {
+            LabelNullaz();
             for (int i = 0; i < ramlabelnevek.Length; i++)
             {
                 labelek[i].Content = ramlabelnevek[i];
+            }
+            if (cBoxRAM.SelectedIndex==cBoxRAM.Items.Count-1)
+            {
+                LabelNullaz();
+            }
+            else
+            {
+                MEMORIA akt = new MEMORIA();
+
+                akt = (MEMORIA)cBoxRAM.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.MEMORIATIPUS;
+                lbl14.Content = akt.SEBESSEG;
+                lbl15.Content = akt.KAPACITAS;
             }
         }
 
         private void rbSSD_Checked(object sender, RoutedEventArgs e)
         {
+            LabelNullaz();
             for (int i = 0; i < tarololabelnevek.Length; i++)
             {
                 labelek[i].Content = tarololabelnevek[i];
+            }
+            if (cBoxSSD.SelectedIndex==cBoxSSD.Items.Count-1)
+            {
+                LabelNullaz();
+            }
+            else
+            {
+                SSD akt = new SSD();
+
+                akt = (SSD)cBoxSSD.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.KAPACITAS;
             }
         }
 
         private void rbTap_Checked(object sender, RoutedEventArgs e)
         {
+            LabelNullaz();
             for (int i = 0; i < taplabelnevek.Length; i++)
             {
                 labelek[i].Content = taplabelnevek[i];
             }
+            if (cBoxTapok.SelectedIndex==cBoxTapok.Items.Count-1)
+            {
+                LabelNullaz();
+            }
+            else
+            {
+                TAP akt = new TAP();
+
+                akt = (TAP)cBoxTapok.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.TELJESITMENY;
+            }
+            
         }
 
         private void rbHaz_Checked(object sender, RoutedEventArgs e)
         {
+            LabelNullaz();
             for (int i = 0; i < hazlabelnevek.Length; i++)
             {
                 labelek[i].Content = hazlabelnevek[i];
+            }
+            if (cBoxHazak.SelectedIndex==cBoxHazak.Items.Count-1)
+            {
+                LabelNullaz();
+            }
+            else
+            {
+                HAZ akt = new HAZ();
+
+                akt = (HAZ)cBoxHazak.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.MERETSZABVANY;
+            }
+            
+        }
+
+        private void cBoxAlaplap_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbAlaplap.IsChecked == true) && cBoxAlaplap.SelectedIndex != cBoxAlaplap.Items.Count - 1)
+            {
+                ALAPLAP aktalaplap = new ALAPLAP();
+                aktalaplap = (ALAPLAP)cBoxAlaplap.SelectedValue;
+
+                lbl11.Content = aktalaplap.AR;
+                lbl12.Content = aktalaplap.TIPUSSZAM;
+                lbl13.Content = aktalaplap.CPUFOGLALAT;
+                lbl14.Content = aktalaplap.MEMORIASLOTOK;
+                lbl15.Content = aktalaplap.MEMORIATIPUS;
+                lbl16.Content = aktalaplap.CHIPSET;
+                lbl17.Content = aktalaplap.MERETSZABVANY;
+
+            }
+            else
+            {
+                LabelNullaz();
+            }
+        }
+
+        private void cBoxCPU_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbProcesszor.IsChecked == true) && cBoxCPU.SelectedIndex != cBoxCPU.Items.Count - 1)
+            {
+                CPU akt = new CPU();
+
+                akt = (CPU)cBoxCPU.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.CPUFOGLALAT;
+                lbl14.Content = akt.FOGYASZTAS;
+                lbl15.Content = akt.SEBESSEG;
+                lbl16.Content = akt.MAGOK;
+
+            }
+            else
+            {
+                LabelNullaz();
+            }
+        }
+
+        private void cBoxHDD_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbWinchester.IsChecked == true) && cBoxHDD.SelectedIndex != cBoxHDD.Items.Count - 1)
+            {
+                //ab = new AdatbazisEntities();
+                HDD aktHDD = new HDD();
+                //string keres = cBoxHDD.SelectedItem.ToString();
+                //var q = from akt in ab.HDD
+                //        where akt.TIPUSSZAM == keres
+                //        select akt;
+
+                //aktHDD = q.FirstOrDefault();
+
+                aktHDD = (HDD)cBoxHDD.SelectedValue;
+
+                lbl11.Content = aktHDD.AR;
+                lbl12.Content = aktHDD.TIPUSSZAM;
+                lbl13.Content = aktHDD.KAPACITAS;
+
+            }
+            else
+            {
+                LabelNullaz();
+            }
+        }
+
+        private void cBoxGPU_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbVideokartya.IsChecked == true) && cBoxGPU.SelectedIndex != cBoxGPU.Items.Count - 1)
+            {
+                GPU akt = new GPU();
+
+                akt = (GPU)cBoxGPU.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.FOGYASZTAS;
+                lbl14.Content = akt.MEMORIA;
+
+            }
+            else
+            {
+                LabelNullaz();
+            }
+        }
+
+        private void cBoxRAM_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbMemoria.IsChecked == true) && cBoxRAM.SelectedIndex != cBoxRAM.Items.Count - 1)
+            {
+                MEMORIA akt = new MEMORIA();
+
+                akt = (MEMORIA)cBoxRAM.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.MEMORIATIPUS;
+                lbl14.Content = akt.SEBESSEG;
+                lbl15.Content = akt.KAPACITAS;
+               
+            }
+            else
+            {
+                LabelNullaz();
+            }
+        }
+
+        private void cBoxSSD_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbSSD.IsChecked == true) && cBoxSSD.SelectedIndex != cBoxSSD.Items.Count - 1)
+            {               
+                SSD akt = new SSD();
+               
+                akt = (SSD)cBoxSSD.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.KAPACITAS;
+
+            }
+            else
+            {
+                LabelNullaz();
+            }
+        }
+
+        private void cBoxTapok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbTap.IsChecked == true) && cBoxTapok.SelectedIndex != cBoxTapok.Items.Count - 1)
+            {
+                TAP akt = new TAP();
+
+                akt = (TAP)cBoxTapok.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.TELJESITMENY;
+
+            }
+            else
+            {
+                LabelNullaz();
+            }
+        }
+
+        private void cBoxHazak_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((rbHaz.IsChecked == true) && cBoxHazak.SelectedIndex != cBoxHazak.Items.Count - 1)
+            {
+                HAZ akt = new HAZ();
+
+                akt = (HAZ)cBoxHazak.SelectedValue;
+
+                lbl11.Content = akt.AR;
+                lbl12.Content = akt.TIPUSSZAM;
+                lbl13.Content = akt.MERETSZABVANY;
+
+            }
+            else
+            {
+                LabelNullaz();
             }
         }
     }
